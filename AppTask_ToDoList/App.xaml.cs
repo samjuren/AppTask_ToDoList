43e4f1e89e0056a1ebf45c633b2542b8c017189b
Samuel Juren <sammjureen@outlook.com>
2024-07-19 10:40:32 -0300

    Estrutura do MVVM, StartPage

--- AppTask_ToDoList/App.xaml.cs
+++ AppTask_ToDoList/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppTask_ToDoList
+﻿using AppTask_ToDoList.Views;
+
+namespace AppTask_ToDoList
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new StartPage());
         }
     }
 }
---
+++ AppTask_ToDoList/Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppTask_ToDoList.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}
