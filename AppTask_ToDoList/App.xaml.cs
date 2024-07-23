26f5ad0bec6c86d502e259f47dd52739e575da3b
Samuel Juren <sammjureen@outlook.com>
2024-07-23 19:16:25 -0300

    Estilização nativa dos componentes

--- AppTask_ToDoList/App.xaml.cs
+++ AppTask_ToDoList/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AppTask_ToDoList.Views;
+﻿
+using AppTask_ToDoList.Views;
+using Microsoft.Maui.Platform;
 
 namespace AppTask_ToDoList
 {
@@ -6,9 +8,27 @@ namespace AppTask_ToDoList
     {
         public App()
         {
+            CustomHandler();
+
             InitializeComponent();
 
             MainPage = new NavigationPage(new StartPage());
+        }
+
+        private void CustomHandler()
+        {
+            Microsoft.Maui.Handlers.EntryHandler
+                .Mapper.AppendToMapping
+                ("NoBorder", (handler, view) =>
+                {
+#if ANDROID
+                    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#elif IOS || MACCATALYST
+                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                    handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+                });
         }
     }
 }
