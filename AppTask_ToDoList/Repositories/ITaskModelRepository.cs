3c74a1bfd3efb24b3277f3370244837b0db44c16
Samuel Juren <sammjureen@outlook.com>
2024-07-31 20:36:57 -0300

    Repositories

---
+++ AppTask_ToDoList/Repositories/ITaskModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using AppTask_ToDoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTask_ToDoList.Repositories
+{
+    interface ITaskModelRepository 
+    {
+        void Add(TaskModel task);
+        void Delete(TaskModel task);
+        IList<TaskModel> GetAll();
+        TaskModel GetById(int id);
+        void Update(TaskModel task);
+    }
+}
---
+++ AppTask_ToDoList/Repositories/TaskModelRepository.cs
@@ -0,0 +1,33 @@
+﻿using AppTask_ToDoList.DbContext;
+using AppTask_ToDoList.Models;
+
+namespace AppTask_ToDoList.Repositories
+{
+    class TaskModelRepositories : ITaskModelRepository
+    {
+        public void Add(TaskModel task)
+        {
+            Database.Connection?.Insert(task);
+        }
+
+        public void Delete(TaskModel task)
+        {
+            Database.Connection?.Delete(task);
+        }
+
+        public IList<TaskModel> GetAll()
+        {
+            return Database.Connection?.Table<TaskModel>().ToList();
+        }
+
+        public TaskModel GetById(int id)
+        {
+            return Database.Connection?.Table<TaskModel>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(TaskModel task)
+        {
+            Database.Connection?.Update(task);
+        }
+    }
+}
