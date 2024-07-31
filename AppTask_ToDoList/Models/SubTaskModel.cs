c7fb02ac318a48105d9e54823bb7a040c824d355
Samuel Juren <sammjureen@outlook.com>
2024-07-31 20:36:33 -0300

    Modelo de dados

---
+++ AppTask_ToDoList/Models/SubTaskModel.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace AppTask_ToDoList.Models
+{
+    [Table("SubTasks")]
+    class SubTaskModel
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ AppTask_ToDoList/Models/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppTask_ToDoList.Models
+{
+    [Table("Tasks")]
+    class TaskModel
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime PrevisonDate { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public bool IsCompleted { get; set; }
+
+        [ForeignKey(nameof(TaskModel))]
+        public List<SubTaskModel> SubTasks { get; set; } = new List<SubTaskModel>();
+    }
+}
